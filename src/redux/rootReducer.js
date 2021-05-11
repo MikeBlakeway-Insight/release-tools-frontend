868c794987d37ccdfeacbffac7a91fce5cbb6d87
MikeBlakeway-Insight <mike.blakeway@insight.com>
2021-05-11 10:17:44 +0100

    removing reducndant redux store

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { reducer as releaseAudits } from './Audits/auditsReducer'
-
-export const rootReducer = combineReducers({
-	releaseAudits,
-})
-
-export default rootReducer
